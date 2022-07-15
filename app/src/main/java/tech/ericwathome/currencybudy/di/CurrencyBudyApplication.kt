5baebbe9cf4b1602f722a6d0536afaf1c0a4bf4b
Eric Wafula <wathomeeric@gmail.com>
2022-07-15 19:10:34 +0300

    added initial hilt setup

---
+++ app/src/main/java/tech/ericwathome/currencybudy/di/CurrencyBudyApplication.kt
@@ -0,0 +1,8 @@
+package tech.ericwathome.currencybudy.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CurrencyBuddyApplication : Application() {
+}
