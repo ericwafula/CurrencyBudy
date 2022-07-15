ff285fb5e60aaa9942607487ef51945669ab3f49
Eric Wafula <wathomeeric@gmail.com>
2022-07-15 23:39:08 +0300

    added Dispatcher provider

--- app/src/main/java/tech/ericwathome/currencybudy/di/AppModule.kt
+++ app/src/main/java/tech/ericwathome/currencybudy/di/AppModule.kt
@@ -4,8 +4,13 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import tech.ericwathome.currencybudy.network.RateApi
 import tech.ericwathome.currencybudy.network.RateClient
+import tech.ericwathome.currencybudy.repository.RateRepository
+import tech.ericwathome.currencybudy.repository.RateRepositoryImpl
+import tech.ericwathome.currencybudy.util.DispatcherProvider
 import javax.inject.Singleton
 
 @Module
@@ -15,5 +20,22 @@ object AppModule {
     @Provides
     @Singleton
     fun provideRateApi(): RateApi = RateClient.rateApi
+
+    @Provides
+    fun provideRateRepository(rateApi: RateApi): RateRepository = RateRepositoryImpl(rateApi)
+
+    @Provides
+    @Singleton
+    fun provideDispatchers(): DispatcherProvider = object : DispatcherProvider {
+        override val main: CoroutineDispatcher
+            get() = Dispatchers.Main
+        override val io: CoroutineDispatcher
+            get() = Dispatchers.IO
+        override val default: CoroutineDispatcher
+            get() = Dispatchers.Default
+        override val unconfined: CoroutineDispatcher
+            get() = Dispatchers.Unconfined
+
+    }
 
 }
---
+++ app/src/main/java/tech/ericwathome/currencybudy/util/DispatcherProvider.kt
@@ -0,0 +1,10 @@
+package tech.ericwathome.currencybudy.util
+
+import kotlinx.coroutines.CoroutineDispatcher
+
+interface DispatcherProvider {
+    val main: CoroutineDispatcher
+    val io: CoroutineDispatcher
+    val default: CoroutineDispatcher
+    val unconfined: CoroutineDispatcher
+}
