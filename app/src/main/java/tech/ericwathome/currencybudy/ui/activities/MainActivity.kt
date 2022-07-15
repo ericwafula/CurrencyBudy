b73305284039e22dad037777165e781928d335ca
Eric Wafula <wathomeeric@gmail.com>
2022-07-16 00:18:49 +0300

    added navController

--- app/src/main/java/tech/ericwathome/currencybudy/ui/activities/MainActivity.kt
+++ app/src/main/java/tech/ericwathome/currencybudy/ui/activities/MainActivity.kt
@@ -2,11 +2,28 @@ package tech.ericwathome.currencybudy.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import tech.ericwathome.currencybudy.R
+import tech.ericwathome.currencybudy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFrag = supportFragmentManager.findFragmentById(R.id.nav_host_frag) as NavHostFragment
+        navController = navHostFrag.navController
+
+        val destinations = setOf(R.id.currencyConverter)
+        val appBarConfiguration = AppBarConfiguration(destinations)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
     }
+
 }
