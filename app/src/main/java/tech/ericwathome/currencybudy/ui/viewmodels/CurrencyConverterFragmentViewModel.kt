f80eb43a1de5e3535bc2a044ef0f7f28a67534a3
Eric Wafula <wathomeeric@gmail.com>
2022-07-16 10:45:36 +0300

    added conversion logic to CurrencyConverterFragmentViewModel

---
+++ app/src/main/java/tech/ericwathome/currencybudy/ui/viewmodels/CurrencyConverterFragmentViewModel.kt
@@ -0,0 +1,55 @@
+package tech.ericwathome.currencybudy.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import tech.ericwathome.currencybudy.repository.RateRepository
+import tech.ericwathome.currencybudy.util.DispatcherProvider
+import tech.ericwathome.currencybudy.util.Resource
+import javax.inject.Inject
+
+@HiltViewModel
+class CurrencyConverterFragmentViewModel @Inject constructor(
+    private val rateRepository: RateRepository,
+    private val dispatchers: DispatcherProvider
+) : ViewModel() {
+
+    sealed class CurrencyEvent {
+        class Success(resultMessage: String): CurrencyEvent()
+        class Failure(errorMessage: String): CurrencyEvent()
+        object Loading : CurrencyEvent()
+        object Empty : CurrencyEvent()
+    }
+
+    private val _conversion = MutableStateFlow<CurrencyEvent>(CurrencyEvent.Empty)
+    val conversion = _conversion
+
+    fun convert(amount: String, from: String, to: String) {
+        val fromAmount = amount.toFloatOrNull()
+        if (fromAmount == null) {
+            _conversion.value = CurrencyEvent.Failure("Invalid amount")
+            return
+        }
+        viewModelScope.launch(dispatchers.io) {
+            _conversion.value = CurrencyEvent.Loading
+            val query = HashMap<String, String>()
+            query["to"] = to
+            query["from"] = from
+            query["amount"] = amount
+            when(val ratesResponse = rateRepository.getExchangeRate(query)) {
+                is Resource.Error -> CurrencyEvent.Failure(ratesResponse.message!!)
+                is Resource.Success -> {
+                    val rate = ratesResponse.response?.result
+                    if (rate == null) {
+                        _conversion.value = CurrencyEvent.Failure("Unexpected Error")
+                    } else {
+                        _conversion.value = CurrencyEvent.Success("$amount $from = $rate $to")
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/tech/ericwathome/currencybudy/ui/viewmodels/LatestFragmentViewModel.kt
@@ -0,0 +1,6 @@
+package tech.ericwathome.currencybudy.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class LatestFragmentViewModel : ViewModel() {
+}
