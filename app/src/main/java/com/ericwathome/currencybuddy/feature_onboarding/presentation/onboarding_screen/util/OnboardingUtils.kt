ebbac54bfa66088d8c90a31b3074de2b2c1b7c37
ericwafula <ericwathome007@gmail.com>
2023-02-15 22:17:57 +0300

    added onboarding utils

--- app/src/main/java/com/ericwathome/currencybuddy/feature_onboarding/presentation/onboarding_screen/util/OnboardingUtils.kt
+++ app/src/main/java/com/ericwathome/currencybuddy/feature_onboarding/presentation/onboarding_screen/util/OnboardingUtils.kt
@@ -2,9 +2,27 @@ package com.ericwathome.currencybuddy.feature_onboarding.presentation.onboarding
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import com.ericwathome.currencybuddy.R
 
 internal data class OnBoardingItem(
     @DrawableRes val image: Int,
     @StringRes val text: Int
 )
+
+internal object OnboardingUtils {
+    val onBoardingItems = listOf(
+        OnBoardingItem(
+            image = R.drawable.onboarding_image_1,
+            text = R.string.onboarding_text_1
+        ),
+        OnBoardingItem(
+            image = R.drawable.onboarding_image_2,
+            text = R.string.onboarding_text_2
+        ),
+        OnBoardingItem(
+            image = R.drawable.onboarding_image_3,
+            text = R.string.onboarding_text_3
+        )
+    )
+}
 
