7803ea4ec75efa541f58d20d5b687954f1062f9a
ericwafula <ericwathome007@gmail.com>
2023-02-15 16:18:26 +0300

    added initial retrofit dependencies in AppModule.kt

--- app/src/main/java/com/ericwathome/currencybuddy/di/AppModule.kt
+++ app/src/main/java/com/ericwathome/currencybuddy/di/AppModule.kt
@@ -1,11 +1,50 @@
 package com.ericwathome.currencybuddy.di
 
+import com.ericwathome.currencybuddy.BuildConfig
+import com.ericwathome.currencybuddy.common.Constants
+import com.ericwathome.currencybuddy.feature_converter.data.data_source.remote.ExchangeRateApiService
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        val logger = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder()
+            .addInterceptor(logger)
+            .connectTimeout(Constants.CALL_TIMEOUT, TimeUnit.SECONDS)
+            .readTimeout(Constants.READ_TIMEOUT, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("${Constants.BASE_URL}${BuildConfig.API_KEY}/")
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideExchangeRateApiService(retrofit: Retrofit): ExchangeRateApiService {
+        return retrofit.create(ExchangeRateApiService::class.java)
+    }
 
 }
--- app/src/main/java/com/ericwathome/currencybuddy/feature_converter/data/data_source/remote/ExchangeRateApiService.kt
+++ app/src/main/java/com/ericwathome/currencybuddy/feature_converter/data/data_source/remote/ExchangeRateApiService.kt
@@ -5,6 +5,6 @@ import com.ericwathome.currencybuddy.feature_converter.data.dto.ExchangeRateDto
 import retrofit2.http.GET
 
 interface ExchangeRateApiService {
-    @GET("{apiKey}/latest/{baseCode}")
+    @GET("latest/{baseCode}")
     suspend fun getLatestRates(apiKey: String, baseCode: String): Resource<ExchangeRateDto>
 }
