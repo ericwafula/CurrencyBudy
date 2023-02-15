1a5684a42915ee366e95dad8fe0cc56cd5380dd6
ericwafula <ericwathome007@gmail.com>
2023-02-15 15:08:42 +0300

    added post notifications permission

--- app/src/main/java/com/ericwathome/currencybuddy/di/CurrencyBuddyApp.kt
+++ app/src/main/java/com/ericwathome/currencybuddy/di/CurrencyBuddyApp.kt
@@ -1,8 +1,23 @@
 package com.ericwathome.currencybuddy.di
 
 import android.app.Application
+import android.app.NotificationManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class CurrencyBuddyApp : Application() {
+    private lateinit var notificationManager: NotificationManager
+    override fun onCreate() {
+        super.onCreate()
+
+        notificationManager = getSystemService(NotificationManager::class.java)
+
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+
+    }
 }
