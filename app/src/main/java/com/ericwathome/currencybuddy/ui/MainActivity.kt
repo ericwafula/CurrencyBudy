78f54503c9ac409be74884f2692c9a7cd4a26418
ericwafula <ericwathome007@gmail.com>
2023-02-15 17:31:31 +0300

    added splashScreen

--- app/src/main/java/com/ericwathome/currencybuddy/ui/MainActivity.kt
+++ app/src/main/java/com/ericwathome/currencybuddy/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.ericwathome.currencybuddy.feature_converter.presentation.converter_screen
+package com.ericwathome.currencybuddy.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,12 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.ericwathome.currencybuddy.feature_converter.presentation.converter_screen.theme.CurrencyBuddyTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             CurrencyBuddyTheme {
