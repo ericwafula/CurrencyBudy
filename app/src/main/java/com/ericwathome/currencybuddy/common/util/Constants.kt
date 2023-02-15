0b1e48a54b2729c0da6b419b9798ca82f9c96468
ericwafula <ericwathome007@gmail.com>
2023-02-15 15:18:14 +0300

    added sync notification channel

---
+++ app/src/main/java/com/ericwathome/currencybuddy/common/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.ericwathome.currencybuddy.common.util
+
+object Constants {
+    const val CHANNEL_SYNC_ID = "sync_id"
+    const val CHANNEL_SYNC_NAME = "Sync Currencies"
+}
--- app/src/main/java/com/ericwathome/currencybuddy/di/CurrencyBuddyApp.kt
+++ app/src/main/java/com/ericwathome/currencybuddy/di/CurrencyBuddyApp.kt
@@ -1,23 +1,29 @@
 package com.ericwathome.currencybuddy.di
 
 import android.app.Application
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.os.Build
 import androidx.core.app.NotificationCompat
+import com.ericwathome.currencybuddy.common.util.Constants
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class CurrencyBuddyApp : Application() {
-    private lateinit var notificationManager: NotificationManager
     override fun onCreate() {
         super.onCreate()
-
-        notificationManager = getSystemService(NotificationManager::class.java)
 
         createNotificationChannel()
     }
 
     private fun createNotificationChannel() {
+        val channel = NotificationChannel(
+            Constants.CHANNEL_SYNC_ID,
+            Constants.CHANNEL_SYNC_NAME,
+            NotificationManager.IMPORTANCE_HIGH
+        )
 
+        val notificationManager = getSystemService(NotificationManager::class.java)
+        notificationManager.createNotificationChannel(channel)
     }
 }
