459d78ae7bde088712897fd240de262262e39041
ericwafula <ericwathome007@gmail.com>
2023-02-23 21:21:52 +0300

    updated ConverterMappers with Currency data class to hold currency name and code

--- app/src/main/java/com/ericwathome/currencybuddy/feature_converter/presentation/converter_screen/ConverterValues.kt
+++ app/src/main/java/com/ericwathome/currencybuddy/feature_converter/presentation/converter_screen/ConverterValues.kt
@@ -1,9 +1,12 @@
 package com.ericwathome.currencybuddy.feature_converter.presentation.converter_screen
 
+import com.ericwathome.currencybuddy.feature_converter.presentation.util.Currency
+
 data class ConverterValues(
-    val currencies: List<String>?,
+    val currencies: List<Currency>?,
     val baseSymbol: String?,
     val baseConversionRate: String?,
     val quoteSymbol: String?,
-    val quotePrice: String?
+    val quotePrice: String?,
+    val currencyName: String?
 )
