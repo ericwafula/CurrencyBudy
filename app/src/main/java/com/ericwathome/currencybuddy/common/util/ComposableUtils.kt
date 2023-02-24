c0fc444c45d30d1e83d4dfe46798db1e4dfc1288
ericwafula <ericwathome007@gmail.com>
2023-02-25 00:14:43 +0300

    added card component and TextSizing

--- app/src/main/java/com/ericwathome/currencybuddy/common/util/ComposableUtils.kt
+++ app/src/main/java/com/ericwathome/currencybuddy/common/util/ComposableUtils.kt
@@ -2,10 +2,7 @@ package com.ericwathome.currencybuddy.common.util
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -13,6 +10,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.window.Dialog
 import com.ericwathome.currencybuddy.R
+import com.ericwathome.currencybuddy.feature_converter.presentation.converter_screen.CreditCardData
 import com.ericwathome.currencybuddy.feature_converter.presentation.converter_screen.theme.CurrencyBuddyTheme
 
 @Composable
@@ -38,10 +36,30 @@ fun ErrorDialog(message: String) {
     }
 }
 
+@Composable
+fun ConverterCard(content: @Composable () -> Unit) {
+    Card(
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary),
+        shape = MaterialTheme.shapes.large
+    ) {
+        content()
+    }
+}
+
 @Preview(uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun AlertDialogTemplatePreview() {
     CurrencyBuddyTheme {
         ErrorDialog(message = "Unable to connect. Check your connection")
+    }
+}
+
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun CurrencyConverterCardPreview() {
+    CurrencyBuddyTheme {
+        ConverterCard {
+            CreditCardData(baseCurrency = "Currency Converter", date = "02/23")
+        }
     }
 }
