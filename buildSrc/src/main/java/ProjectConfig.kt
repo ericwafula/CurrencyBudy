18663f3c98d709bdf9a53ed861b276c6526c913e
ericwafula <ericwathome007@gmail.com>
2023-03-03 20:22:22 +0300

    added buildSrc directory

---
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -0,0 +1,8 @@
+object ProjectConfig {
+    const val COMPILE_SDK = 33
+    const val MIN_SDK = 26
+    const val TARGET_SDK = 33
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "1.0"
+    const val KOTLIN_COMPILER_EXTENSION_VERSION = "1.4.2"
+}
