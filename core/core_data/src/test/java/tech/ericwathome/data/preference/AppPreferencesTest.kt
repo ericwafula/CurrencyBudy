37aefb6d1c5a5f8dc7a067017e65442c1e2a99a0
ericwafula <ericwathome007@gmail.com>
2023-03-12 18:48:21 +0300

    check if the appPreferences interface's getOnBoarding status is invoked correctly

--- core/core_data/src/test/java/tech/ericwathome/data/preference/AppPreferencesTest.kt
+++ core/core_data/src/test/java/tech/ericwathome/data/preference/AppPreferencesTest.kt
@@ -5,11 +5,13 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import app.cash.turbine.test
 import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
-import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
 
 @RunWith(AndroidJUnit4::class)
 class AppPreferencesTest {
@@ -35,6 +37,34 @@ class AppPreferencesTest {
         appPreferencesImpl.getOnboardingStatus().test {
             val onBoardingStatus = awaitItem()
             assertThat(onBoardingStatus).isTrue()
+        }
+    }
+
+    @Test
+    fun `check if the appPreferences interface's getOnBoarding status is invoked correctly, returns true`() = runBlocking {
+        val appPreferences = mock(AppPreferences::class.java)
+        Mockito.`when`(appPreferences.getOnboardingStatus())
+            .thenReturn(flowOf(true))
+        appPreferences.getOnboardingStatus().test {
+            val onBoardingStatus = awaitItem()
+            assertThat(onBoardingStatus).isTrue()
+
+            // ensure there are no more events
+            expectNoEvents()
+        }
+    }
+
+    @Test
+    fun `check if the appPreferences interface's getOnBoarding status is invoked correctly, returns false`() = runBlocking {
+        val appPreferences = mock(AppPreferences::class.java)
+        Mockito.`when`(appPreferences.getOnboardingStatus())
+            .thenReturn(flowOf(false))
+        appPreferences.getOnboardingStatus().test {
+            val onBoardingStatus = awaitItem()
+            assertThat(onBoardingStatus).isFalse()
+
+            // ensure there are no more events
+            expectNoEvents()
         }
     }
 
