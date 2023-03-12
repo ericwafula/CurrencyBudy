fe8bcf106956180df7bdeb4f0593f6497cc5e758
ericwafula <ericwathome007@gmail.com>
2023-03-12 18:27:55 +0300

    added test to ensure default onBoarding status is updated correctly

--- core/core_data/src/test/java/tech/ericwathome/data/preference/AppPreferencesTest.kt
+++ core/core_data/src/test/java/tech/ericwathome/data/preference/AppPreferencesTest.kt
@@ -7,21 +7,34 @@ import app.cash.turbine.test
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class AppPreferencesTest {
+    private lateinit var appPreferencesImpl: AppPreferencesImpl
+
+    @Before
+    fun setup() {
+        val context: Context = ApplicationProvider.getApplicationContext()
+        appPreferencesImpl = AppPreferencesImpl(context)
+    }
 
     @Test
     fun `ensure default onBoarding status value is false`() = runBlocking {
-        val context: Context = ApplicationProvider.getApplicationContext()
-
-        val appPreferencesImpl = AppPreferencesImpl(context)
-
         appPreferencesImpl.getOnboardingStatus().test {
             val onBoardingStatus = awaitItem()
             assertThat(onBoardingStatus).isFalse()
+        }
+    }
+
+    @Test
+    fun `check if onBoarding status gets updated to true`() = runBlocking {
+        appPreferencesImpl.updateOnboardingStatus(true)
+        appPreferencesImpl.getOnboardingStatus().test {
+            val onBoardingStatus = awaitItem()
+            assertThat(onBoardingStatus).isTrue()
         }
     }
 
