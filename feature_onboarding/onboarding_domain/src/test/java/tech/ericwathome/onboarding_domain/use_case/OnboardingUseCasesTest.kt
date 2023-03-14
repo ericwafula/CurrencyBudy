83729d5819b5f1c4d08fd7ad3d018235f861c9a7
ericwafula <ericwathome007@gmail.com>
2023-03-14 08:54:14 +0300

    added test to ensure appPreferences interface's updateOnboarding status is invoked when the UpdateOnboardingUseCase is invoked

--- feature_onboarding/onboarding_domain/src/test/java/tech/ericwathome/onboarding_domain/use_case/OnboardingUseCasesTest.kt
+++ feature_onboarding/onboarding_domain/src/test/java/tech/ericwathome/onboarding_domain/use_case/OnboardingUseCasesTest.kt
@@ -2,8 +2,11 @@ package tech.ericwathome.onboarding_domain.use_case
 
 import app.cash.turbine.test
 import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.InjectMocks
@@ -18,13 +21,17 @@ class OnboardingUseCasesTest {
 
     @Mock
     private lateinit var appPreferences: AppPreferences
+    @Mock
+    private lateinit var getOnBoardingStatus: GetOnboardingStatus
+    @Mock
+    private lateinit var updateOnBoardingStatus: UpdateOnboardingStatus
+
 
     @Test
     fun `ensure the getOnBoardingStatus() returns the correct values, returns true`() = runBlocking {
-        var getOnboardingStatus = mock(GetOnboardingStatus::class.java)
-        Mockito.`when`(getOnboardingStatus.invoke())
+        Mockito.`when`(getOnBoardingStatus.invoke())
             .thenReturn(flowOf(true))
-        getOnboardingStatus().test {
+        getOnBoardingStatus().test {
             val onBoardingStatus = awaitItem()
             assertThat(onBoardingStatus).isTrue()
             expectNoEvents()
@@ -33,7 +40,6 @@ class OnboardingUseCasesTest {
 
     @Test
     fun `ensure the getOnBoardingStatus() returns the correct values, returns false`() = runBlocking {
-        var getOnBoardingStatus = mock(GetOnboardingStatus::class.java)
         Mockito.`when`(getOnBoardingStatus.invoke())
             .thenReturn(flowOf(false))
         getOnBoardingStatus().test {
@@ -45,7 +51,7 @@ class OnboardingUseCasesTest {
 
     @Test
     fun `ensure getOnBoardingStatus() returns the correct value, returns true when appPreferences has a true value`() = runBlocking{
-        var getOnBoardingStatus = GetOnboardingStatus(appPreferences)
+        val getOnBoardingStatus = GetOnboardingStatus(appPreferences)
         Mockito.`when`(appPreferences.getOnboardingStatus())
             .thenReturn(flowOf(true))
         appPreferences.getOnboardingStatus()
@@ -58,7 +64,7 @@ class OnboardingUseCasesTest {
 
     @Test
     fun `ensure getOnBoardingStatus() returns the correct value, returns false when appPreferences has a false value`() = runBlocking{
-        var getOnBoardingStatus = GetOnboardingStatus(appPreferences)
+        val getOnBoardingStatus = GetOnboardingStatus(appPreferences)
         Mockito.`when`(appPreferences.getOnboardingStatus())
             .thenReturn(flowOf(false))
         appPreferences.getOnboardingStatus()
@@ -69,6 +75,12 @@ class OnboardingUseCasesTest {
         }
     }
 
-
+    @Test
+    fun `ensure updateOnBoardingStatus() in the appPreferences interface is invoked`() = runBlocking {
+        val updateOnboardingStatus = UpdateOnboardingStatus(appPreferences)
+        updateOnboardingStatus(true)
+        Mockito.verify(appPreferences, Mockito.times(1))
+            .updateOnboardingStatus(true)
+    }
 
 }
