eea9547a5a4667c0bf2670d6040e10949758356e
ericwafula <ericwathome007@gmail.com>
2023-03-14 18:37:36 +0300

    added crashHandler

--- app/src/main/java/com/ericwathome/currencybuddy/di/CurrencyBuddyApp.kt
+++ app/src/main/java/com/ericwathome/currencybuddy/di/CurrencyBuddyApp.kt
@@ -1,13 +1,17 @@
 package com.ericwathome.currencybuddy.di
 
 import android.app.Application
+import android.os.Handler
+import android.os.Looper
 import com.ericwathome.currencybuddy.BuildConfig
+import com.ericwathome.currencybuddy.exceptions.CrashListener
 import tech.ericwathome.presentation.util.NotificationUtils
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
-class CurrencyBuddyApp : Application(), NotificationUtils.InitNotifications by NotificationUtils.Notifications() {
+class CurrencyBuddyApp : Application(),
+    NotificationUtils.InitNotifications by NotificationUtils.Notifications(), CrashListener {
     companion object {
         val TAG = this::class.simpleName
     }
@@ -20,5 +24,31 @@ class CurrencyBuddyApp : Application(), NotificationUtils.InitNotifications by N
         }
 
         createNotificationChannel(this)
+        setupCrashHandler()
+    }
+
+    override fun uncaughtException(thread: Thread, throwable: Throwable) {
+        NotificationUtils.showNotification(
+            this,
+            "Something went wrong!",
+            "We are working on it"
+        )
+        Timber.tag("$TAG").d("$throwable")
+        // TODO setup crashlytics
+    }
+
+    private fun setupCrashHandler() {
+        Handler(Looper.getMainLooper()).post {
+            while (true) {
+                try {
+                    Looper.loop()
+                } catch (e: Throwable) {
+                    uncaughtException(Looper.getMainLooper().thread, e)
+                }
+            }
+        }
+        Thread.setDefaultUncaughtExceptionHandler { t, e ->
+            uncaughtException(t, e)
+        }
     }
 }
